b5ca1d8bbc8db43c82449503ed4dad945fee78e3
jderache <contact@julienderache.fr>
2023-04-09 17:15:45 +0200

    Ajout balise manquante

--- wp-content/themes/jujuTheme/archive.php
+++ wp-content/themes/jujuTheme/archive.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
-<h1 class="page-title"><?php single_cat_title(); ?></h1>
-<section class="posts">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <article class="post">
-                <h2><?php the_title(); ?></h2>
+<div>
+    <h1 class="page-title"><?php single_cat_title(); ?></h1>
+    <section class="posts">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article class="post">
+                    <h2><?php the_title(); ?></h2>
 
-                <?php the_post_thumbnail(); ?>
+                    <?php the_post_thumbnail(); ?>
 
-                <p class="post__meta">
-                    Publié le <?php the_time(get_option('date_format')); ?> - <?php the_category($separator = '-'); ?> - <?php comments_number(); ?>
-                </p>
+                    <p class="post__meta">
+                        Publié le <?php the_time(get_option('date_format')); ?> - <?php the_category($separator = '-'); ?> - <?php comments_number(); ?>
+                    </p>
 
-                <?php the_excerpt(); ?>
+                    <?php the_excerpt(); ?>
 
-                <p>
-                    <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-                </p>
-            </article>
-    <?php endwhile;
-    endif; ?>
-</section>
+                    <p>
+                        <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
+                    </p>
+                </article>
+        <?php endwhile;
+        endif; ?>
+    </section>
+</div>
 <?php get_footer(); ?>
