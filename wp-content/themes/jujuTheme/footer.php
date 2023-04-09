3c514a472ae641309e6d0aebe4d9ddb8d7492d62
jderache <contact@julienderache.fr>
2023-04-09 16:33:37 +0200

    Ajout du plugin de compteur de pages vues

--- wp-content/themes/jujuTheme/footer.php
+++ wp-content/themes/jujuTheme/footer.php
@@ -1,5 +1,4 @@
 <footer class="site__footer">
+    <?php wp_footer(); ?>
     Développé par Julien DERACHE - Copyright &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
 </footer>
-
-<?php wp_footer(); ?>
--- wp-content/themes/jujuTheme/home.php
+++ wp-content/themes/jujuTheme/home.php
@@ -1,30 +1,37 @@
 <?php get_header(); ?>
-<h1>Films à l'affiche</h1>
-<section class="posts">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div>
+    <h1>Films à l'affiche</h1>
+    <?php add_action('wp_footer', 'afficher_compteur_pages_vues'); ?>
+    <?php
+    // Action pour incrémenter le compteur de pages vues à chaque chargement de page
+    add_action('wp_head', 'incrementer_compteur_pages_vues');
+    ?>
+    <section class="posts">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-            <article class="post">
-                <a href="<?php the_permalink(); ?>" class="">
-                    <h2><?php the_title(); ?></h2>
-                    <p>Catégorie <?php the_category($separator = '-'); ?></p>
+                <article class="post">
+                    <a href="<?php the_permalink(); ?>" class="">
+                        <h2><?php the_title(); ?></h2>
+                        <p>Catégorie <?php the_category($separator = '-'); ?></p>
 
-                    <?php the_post_thumbnail(); ?>
+                        <?php the_post_thumbnail(); ?>
 
-                    <p class="post__meta">
-                        Publié le <?php the_time(get_option('date_format')); ?> - <?php the_category($separator = '-'); ?> - <?php comments_number(); ?>
-                    </p>
+                        <p class="post__meta">
+                            Publié le <?php the_time(get_option('date_format')); ?> - <?php the_category($separator = '-'); ?> - <?php comments_number(); ?>
+                        </p>
 
-                    <?php the_excerpt(); ?>
+                        <?php the_excerpt(); ?>
 
-                    <p>
-                        <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
-                    </p>
-                </a>
-            </article>
+                        <p>
+                            <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
+                        </p>
+                    </a>
+                </article>
 
 
-    <?php endwhile;
+        <?php endwhile;
 
-    endif; ?>
-</section>
+        endif; ?>
+    </section>
+</div>
 <?php get_footer(); ?>
